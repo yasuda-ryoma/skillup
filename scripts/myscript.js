75e6aa61f1e81ea3d17058ab032e0e2a0bde1830
Yuichiro Itaya <mr.itaya.magic@gmail.com>
2019-02-08 18:39:06 +0900

    fix: ページ遷移した時にjsを実行し直す

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -20,14 +20,19 @@ cheat.add( [38, 38, 40, 40, 37, 39, 37, 39, 66, 65], function() {
 		e.innerHTML = ""
 	}
 });
-
-$('code').each(function(index,element){
-	var class_name = $(element).attr('class');
-	if (class_name) {
-		if (class_name.match(':')) {
-			var file_name = class_name.split(':')[1];
-			console.log(class_name);
-			$(this).before('<span style="background-color: gainsboro;font-size: small;position: relative;top: -20px;left: -10px;">'+file_name+'</span><br>');
+function code_exchange(){
+	$('code').each(function(index,element){
+		var class_name = $(element).attr('class');
+		$(element).removeClass(class_name);
+		if (class_name) {
+			if (class_name.match(':')) {
+				var file_name = class_name.split(':')[1];
+				$(this).before('<span class="is_chenge" style="background-color: gainsboro;font-size: small;position: relative;top: -20px;left: -10px;">'+file_name+'</span><br>');
+			}
 		}
-	}
-})
+	})	
+}
+$('head').on('DOMSubtreeModified propertychange', function() {
+       code_exchange();
+});
+code_exchange();
